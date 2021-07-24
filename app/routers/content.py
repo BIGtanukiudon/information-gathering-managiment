3eb10000639bb2e691b4fc339715b853fedca8cd
BIGtanukiudon <rksakanatanuki@gmail.com>
2021-07-24 22:01:19 +0900

    feat: 当日の記事のみ登録するように機能変更。 #9

--- app/routers/content.py
+++ app/routers/content.py
@@ -1,10 +1,9 @@
-from datetime import datetime
+from datetime import date, datetime
 from typing import List
 from utils.scraping import scraping_contents as sc
 import utils.crud_collection_destination as crud_cd
 import utils.crud_content as crud_c
 from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.responses import JSONResponse
 from models.content import ContentCreate as CC
 from models.scraping import ScrapingContent as SCM
 from database.config import SessionLocal
@@ -44,17 +43,19 @@ async def scraping_contents(db: Session = Depends(get_db)):
             collection_destination.content_url_attr_name)
 
         for scraping_content in scraping_contents:
-            content = CC(
-                title=scraping_content.title,
-                content_url=scraping_content.content_url,
-                published_at=scraping_content.published_at,
-                domain=scraping_content.domain,
-                is_read_later=False,
-                collection_destination_id=cd_id,
-                account_id=account_id
-            )
-
-            create_content_list.append(content)
+            today = date.today()
+            # 当日の日付のもののみ登録
+            if today == datetime.date(scraping_content.published_at):
+                content = CC(
+                    title=scraping_content.title,
+                    content_url=scraping_content.content_url,
+                    published_at=scraping_content.published_at,
+                    domain=scraping_content.domain,
+                    is_read_later=False,
+                    collection_destination_id=cd_id,
+                    account_id=account_id
+                )
+                create_content_list.append(content)
 
     if len(create_content_list) == 0:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
