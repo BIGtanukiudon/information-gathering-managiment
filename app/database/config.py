6b890e1d36f2aa65f21e1997e9040ad0f8c37caf
BIGtanukiudon <rksakanatanuki@gmail.com>
2021-07-28 09:48:32 +0900

    refactor: envファイルの場所を変更する。

--- app/database/config.py
+++ app/database/config.py
@@ -1,24 +1,10 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import config.env as env
 
 
-load_dotenv(verbose=True)
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-POSTGRES_SERVER = os.environ.get("POSTGRES_SERVER")
-POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-POSTGRES_DB = os.environ.get("POSTGRES_DB")
-
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = f"postgresql://{env.POSTGRES_USER}:{env.POSTGRES_PASSWORD}@{env.POSTGRES_SERVER}:{env.POSTGRES_PORT}/{env.POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
