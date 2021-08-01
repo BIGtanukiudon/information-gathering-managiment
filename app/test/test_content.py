b7b817441382eac4d7ce5f8b8390bb1724688d96
BIGtanukiudon <rksakanatanuki@gmail.com>
2021-08-01 22:52:56 +0900

    feat: スクレイピングのテスト用APIを作成。

--- app/test/test_content.py
+++ app/test/test_content.py
@@ -17,3 +17,19 @@ def test_get_content_list():
     res_json = response.json()
     assert response.status_code == 200
     assert len(res_json) > 0
+
+
+def test_test_scraping_contents():
+    response = client.post(
+        f"{api_endpoint}/test_scraping_contents/",
+        json={
+            "name": "test",
+            "domain": "domain",
+            "contents_attr_name": "contents_attr_name",
+            "title_attr_name": "title_attr_name",
+            "published_date_attr_name": "published_date_attr_name",
+            "content_url_attr_name": "content_url_attr_name",
+            "account_id": 1})
+    res_json = response.json()
+    assert response.status_code == 200
+    assert len(res_json) > 0
